071b9094470afcce6efd26b310cfeda2a0ab63e1
Matthieu Bosquet <matthieubosquet@gmail.com>
2026-02-06 15:52:41 +0000

    Add ical vocabulary

---
+++ src/vocabulary/ical.ts
@@ -0,0 +1,7 @@
+export const ICAL = {
+  comment: "http://www.w3.org/2002/12/cal/ical#comment",
+  dtend: "http://www.w3.org/2002/12/cal/ical#dtend",
+  dtstart: "http://www.w3.org/2002/12/cal/ical#dtstart",
+  location: "http://www.w3.org/2002/12/cal/ical#location",
+  summary: "http://www.w3.org/2002/12/cal/ical#summary",
+} as const;
