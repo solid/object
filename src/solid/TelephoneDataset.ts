91e63a45c5bebbcde71f96b6b23891639f4f6c04
Huseyin Kir <73784267+hkir-dev@users.noreply.github.com>
2026-02-26 13:08:18 +0000

    typo fix

--- src/solid/TelephoneDataset.ts
+++ src/solid/TelephoneDataset.ts
@@ -1,6 +1,6 @@
 import { DatasetWrapper } from 'rdfjs-wrapper';
 import { VCARD } from '../vocabulary/vcard.js';
-import { Telephone } from './Telephone';
+import { Telephone } from './Telephone.js';
 
 export class TelephoneDataset extends DatasetWrapper {
   get telephone(): Iterable<Telephone> {
