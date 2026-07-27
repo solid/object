3a33b3ea1341ddf809395ab36c90145cdf335d05
Precious Oritsedere <rukyjacob@gmail.com>
2026-07-27 11:19:30 +0100

    Add LDP container type IRIs to vocabulary

--- src/vocabulary/ldp.ts
+++ src/vocabulary/ldp.ts
@@ -1,3 +1,5 @@
 export const LDP = {
   contains: "http://www.w3.org/ns/ldp#contains",
+  Container: "http://www.w3.org/ns/ldp#Container",
+  BasicContainer: "http://www.w3.org/ns/ldp#BasicContainer",
 } as const;
